f2c0d50739d7d5f1847dffa77b2ecd1d0b09dc89
BVSanthosh <sb409@st-andrews.ac.uk>
2025-03-04 22:01:33 +0000

    changed brand name

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -11,7 +11,7 @@ const NoChatSelected = () => {
                         </div>
                     </div>
                 </div>
-                <h2 className="text-2xl font-bold">Welcome to Chatty!</h2>
+                <h2 className="text-2xl font-bold">Welcome to Chatcast!</h2>
                 <p className="text-base-content/60">Select a conversation from the sidebar to start chatting</p>
             </div>
         </div>
